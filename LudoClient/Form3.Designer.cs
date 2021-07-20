be048c684a4205b7c05eb1ddc164a0a5503ccf60
Marcelo Rios <chelo38@gmail.com>
2021-07-20 19:31:47 -0400

    Implementacion del Cliente con paso de turnos

---
+++ LudoClient/Form3.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace LudoClient
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.CloseGameButton = new System.Windows.Forms.Button();
+            this.BoardPictureBox = new System.Windows.Forms.PictureBox();
+            this.BlocksImageList = new System.Windows.Forms.ImageList(this.components);
+            this.TurnLabel = new System.Windows.Forms.Label();
+            this.EndTurnButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.BoardPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CloseGameButton
+            // 
+            this.CloseGameButton.Location = new System.Drawing.Point(713, 12);
+            this.CloseGameButton.Name = "CloseGameButton";
+            this.CloseGameButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseGameButton.TabIndex = 0;
+            this.CloseGameButton.Text = "Cerrar";
+            this.CloseGameButton.UseVisualStyleBackColor = true;
+            this.CloseGameButton.Click += new System.EventHandler(this.CloseGameButton_Click);
+            // 
+            // BoardPictureBox
+            // 
+            this.BoardPictureBox.Location = new System.Drawing.Point(98, 18);
+            this.BoardPictureBox.Name = "BoardPictureBox";
+            this.BoardPictureBox.Size = new System.Drawing.Size(420, 420);
+            this.BoardPictureBox.TabIndex = 1;
+            this.BoardPictureBox.TabStop = false;
+            // 
+            // BlocksImageList
+            // 
+            this.BlocksImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("BlocksImageList.ImageStream")));
+            this.BlocksImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.BlocksImageList.Images.SetKeyName(0, "block1.png");
+            this.BlocksImageList.Images.SetKeyName(1, "block2.png");
+            this.BlocksImageList.Images.SetKeyName(2, "block3.png");
+            this.BlocksImageList.Images.SetKeyName(3, "block4.png");
+            this.BlocksImageList.Images.SetKeyName(4, "block5.png");
+            this.BlocksImageList.Images.SetKeyName(5, "block6.png");
+            // 
+            // TurnLabel
+            // 
+            this.TurnLabel.AutoSize = true;
+            this.TurnLabel.Location = new System.Drawing.Point(640, 129);
+            this.TurnLabel.Name = "TurnLabel";
+            this.TurnLabel.Size = new System.Drawing.Size(58, 13);
+            this.TurnLabel.TabIndex = 2;
+            this.TurnLabel.Text = "Esperando";
+            this.TurnLabel.Visible = false;
+            // 
+            // EndTurnButton
+            // 
+            this.EndTurnButton.Location = new System.Drawing.Point(633, 162);
+            this.EndTurnButton.Name = "EndTurnButton";
+            this.EndTurnButton.Size = new System.Drawing.Size(75, 23);
+            this.EndTurnButton.TabIndex = 3;
+            this.EndTurnButton.Text = "Terminar";
+            this.EndTurnButton.UseVisualStyleBackColor = true;
+            this.EndTurnButton.Visible = false;
+            this.EndTurnButton.Click += new System.EventHandler(this.EndTurnButton_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EndTurnButton);
+            this.Controls.Add(this.TurnLabel);
+            this.Controls.Add(this.BoardPictureBox);
+            this.Controls.Add(this.CloseGameButton);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.BoardPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseGameButton;
+        private System.Windows.Forms.PictureBox BoardPictureBox;
+        private System.Windows.Forms.ImageList BlocksImageList;
+        private System.Windows.Forms.Label TurnLabel;
+        private System.Windows.Forms.Button EndTurnButton;
+    }
+}
---
+++ LudoClient/Form3.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameClient;
+
+namespace LudoClient
+{
+    enum GameBoardFields
+    {
+        Red,
+        Yellow,
+        Green,
+        Blue,
+        Orange,
+        Violet,
+    }
+    public partial class Form3 : Form, IGClient3
+    {
+        // Properties
+        private GClient GameClient;
+        private static Graphics drawer;
+        private static GameBoardFields[,] GameBoard;
+        private static int blockSize = 35;
+
+
+        public Form3(GClient GameClient, String Game)
+        {
+            InitializeComponent();
+            this.GameClient = GameClient;
+            GameClient.RegisterObserver3(this);
+            GameClient.StartGame(Game);
+        }
+
+        public Form3(GClient GameClient, int GameId)
+        {
+            InitializeComponent();
+            this.GameClient = GameClient;
+            GameClient.RegisterObserver3(this);
+            GameClient.JoinGame(GameId);
+        }
+
+        // Control Events
+        private void CloseGameButton_Click(object sender, EventArgs e)
+        {
+            Invoke(LaunchMenuFormHandler, this);
+            Invoke(CloseFormHandler, this);
+        }
+
+        private void EndTurnButton_Click(object sender, EventArgs e)
+        {
+            EndTurnButton.Visible = false;
+            this.GameClient.EndTurn();
+        }
+
+        // IGCLient3
+        void IGClient3.OnGBoard(GBoardEvent e)
+        {
+            Invoke(DrawBoardHandler, this);
+        }
+
+        void IGClient3.OnGUpdate(GUpdateEvent e)
+        {
+            Invoke(UpdateHandler, this);
+        }
+
+        // Form Control
+        private delegate void CloseFormDelegate(Form3 form);
+        CloseFormDelegate CloseFormHandler = CloseForm;
+        private static void CloseForm(Form3 form)
+        {
+            form.BoardPictureBox.Refresh();
+            form.Dispose();
+        }
+
+        private delegate void LaunchMenuFormDelegate(Form3 form);
+        LaunchMenuFormDelegate LaunchMenuFormHandler = LaunchMenuForm;
+        private static void LaunchMenuForm(Form3 form)
+        {
+            Form2 menu = new Form2(form.GameClient);
+            menu.Show();
+        }
+
+        private delegate void DrawBoardDelegate(Form3 form);
+        DrawBoardDelegate DrawBoardHandler = DrawBoard;
+        public static void DrawBoard(Form3 form)
+        {
+            int size = GClient.Match.board.size;
+            string[] cells = GClient.Match.board.cells;
+            GameBoard = new GameBoardFields[size, size];
+            form.BoardPictureBox.Image = new Bitmap(size * blockSize, size * blockSize);
+            drawer = Graphics.FromImage(form.BoardPictureBox.Image);
+            drawer.Clear(Color.Black);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int pos = (size * i) + j;
+                    drawer.DrawImage(form.BlocksImageList.Images[int.Parse(cells[pos])], j * blockSize, i * blockSize);
+                }
+            }
+        }
+
+        private delegate void UpdateDelegate(Form3 form);
+        UpdateDelegate UpdateHandler = UpdateBoard;
+        public static void UpdateBoard(Form3 form)
+        {
+            if (GClient.Match.playing)
+            {
+                form.TurnLabel.Visible = false;
+                form.EndTurnButton.Visible = true;
+            }
+            else
+            {
+                form.TurnLabel.Text = "Esperando";
+                form.TurnLabel.Visible = true;
+                form.EndTurnButton.Visible = false;
+            }
+        }
+    }
+}
